edb2f9e539038d88a0814561f8544f915c8e489e
Denali Balser <denali.balser167@gmail.com>
2019-02-20 17:31:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,6 @@
 def speak_to_grandma(string)
 
-  if speak_to_grandma == "I LOVE YOU GRANDMA!"
+string = "I LOVE YOU GRANDMA!"
+  if string == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   end
