99741e9318307ce0b993ae558b319503b8f33f06
Denali Balser <denali.balser167@gmail.com>
2019-02-20 18:31:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(string)
-  if string == string.upcase()
-    return "NO, NOT SINCE 1938!"
+  if string == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU PUMPKIN!"
   elsif string == string.downcase()
     return "HUH?! SPEAK UP, SONNY!"
-  elsif string == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU PUMPKIN!"
+  elsif string == string.upcase()
+    return "NO, NOT SINCE 1938!"
   end
 end
